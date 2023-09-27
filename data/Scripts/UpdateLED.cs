a76fc018e20a24e403be630f2bf8c7efafdf2a7e
Anthogonyst <33465817+Anthogonyst@users.noreply.github.com>
2023-09-27 00:39:34 -0400

    Add test files for yaml to json

---
+++ data/Scripts/UpdateLED.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateLED : MonoBehaviour {
+
+	public int offset = 50;
+	public SpriteRenderer led;
+	public float waitTimeTillOff = 4f;
+	public List<Sprite> ledScreens;
+	private bool safety = true;
+	private bool screenIsOn = false;
+	
+	void Start() {
+		if (led == null || ledScreens.Count != 13) {
+			Debug.Log("Incorrectly instantiated LED screen. " + transform.name);
+			safety = false;
+		}
+	}
+	
+	public bool displayPianoKey(int n) {
+		if (safety) {
+			n = (n + offset) % 12;
+			led.sprite = ledScreens[n];
+			screenIsOn = true;
+			
+			CancelInvoke();
+			Invoke("turnOffLED", waitTimeTillOff);
+			return true;
+		}
+		return false;
+	}
+	
+	public bool turnOffLED() {
+		if (safety) {
+			led.sprite = ledScreens[12];
+			screenIsOn = false;
+			return true;
+		}
+		return false;
+	}
+	
+	public bool isOn() {
+		return screenIsOn;
+	}
+}
